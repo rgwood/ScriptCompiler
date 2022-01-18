df03b64df850a8cfab759a6a83da663b8c2a57ff
Reilly Wood <reilly.wood@icloud.com>
2022-01-17 16:00:05 -0800

    Remove now-unused scripts folder

--- scripts/Program.cs
+++
@@ -1,1 +0,0 @@
-Console.WriteLine("Hello, World! This is Program.cs");
--- scripts/curl.cs
+++
@@ -1,4 +0,0 @@
-Console.WriteLine("Getting example.com...");
-var client = new HttpClient();
-var response = await client.GetStringAsync("https://example.com");
-Console.WriteLine(response);
