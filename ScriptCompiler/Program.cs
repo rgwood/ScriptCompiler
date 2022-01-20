3a17e19b762cf10c2b3232a062c5ad0e5dd903f5
Reilly Wood <reilly.wood@icloud.com>
2022-01-19 20:37:58 -0800

    fix dotnet watch with CliFx

--- ScriptCompiler/Program.cs
+++ ScriptCompiler/Program.cs
@@ -4,6 +4,7 @@ using CliFx.Infrastructure;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Spectre.Console;
 using Utils;
 
 namespace ScriptCompiler;
@@ -17,19 +18,15 @@ public static class Program
             .Build()
             .RunAsync();
     }
-}
 
-[Command]
-public class DefaultCommand : ICommand
-{
-    public async ValueTask ExecuteAsync(IConsole console)
+    public static async Task Startup()
     {
         using var log = new LoggerConfiguration()
             .WriteTo.File("ScriptCompiler.log", rollingInterval: RollingInterval.Day)
             .CreateLogger();
         Log.Logger = log;
         Log.Information("Starting up...");
-        console.Output.WriteLine("Starting up...");
+        AnsiConsole.WriteLine("Starting up...");
 
         DotEnv.Load(".env");
         DotEnv.Load(".env.scriptcompiler"); // in case there are other apps in the same dir as ScriptCompiler
@@ -59,11 +56,21 @@ public class DefaultCommand : ICommand
     }
 }
 
+[Command]
+public class DefaultCommand : ICommand
+{
+    public async ValueTask ExecuteAsync(IConsole console) => await Program.Startup();
+}
+
+// needed because dotnet watch adds an implicit `run` arg
+[Command("run")]
+public class RunCommand : ICommand
+{
+    public async ValueTask ExecuteAsync(IConsole console) => await Program.Startup();
+}
+
 [Command("install")]
 public class InstallCommand : ICommand
 {
-    public async ValueTask ExecuteAsync(IConsole console)
-    {
-        await Systemd.InstallServiceAsync();
-    }
+    public async ValueTask ExecuteAsync(IConsole console) => await Systemd.InstallServiceAsync();
 }
